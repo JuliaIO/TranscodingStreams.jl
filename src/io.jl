b386bc9982596da33da66cdde1285e3d1ccb4384
Kenta Sato <bicycle1885@gmail.com>
2017-05-09 10:24:19 +0900

    add docstring to unsafe_read

--- src/io.jl
+++ src/io.jl
@@ -1,16 +1,29 @@
-function unsafe_read(stream::IO, ptr::Ptr{UInt8}, nbytes::Int)::Int
+# IO Functions
+# ------------
+
+"""
+    unsafe_read(input::IO, output::Ptr{UInt8}, nbytes::Int)::Int
+
+Copy at most `nbytes` from `input` into `output`.
+
+This function is similar to `Base.unsafe_read` but is different in some points:
+- It does not throw `EOFError` when it fails to read `nbytes` from `input`.
+- It returns the number of bytes written to `output`.
+- It does not block if there are buffered data in `input`.
+"""
+function unsafe_read(input::IO, output::Ptr{UInt8}, nbytes::Int)::Int
     nread = 0
-    navail = nb_available(stream)
-    if navail == 0 && nbytes > 0 && !eof(stream)
-        b = read(stream, UInt8)
-        unsafe_store!(ptr, b)
-        ptr += 1
+    navail = nb_available(input)
+    if navail == 0 && nbytes > 0 && !eof(input)
+        b = read(input, UInt8)
+        unsafe_store!(output, b)
+        output += 1
         nbytes -= 1
         nread += 1
-        navail = nb_available(stream)
+        navail = nb_available(input)
     end
     n = min(navail, nbytes)
-    Base.unsafe_read(stream, ptr, n)
+    Base.unsafe_read(input, output, n)
     nread += n
     return nread
 end
