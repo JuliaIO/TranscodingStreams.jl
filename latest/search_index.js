f479249e0850ba8b5912e541f3f62d1979cf35e3
autodocs <autodocs>
2017-06-05 16:00:41 +0000

    build based on 07ef5f4

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "TranscodingStreams.jl",
     "title": "Introduction",
     "category": "section",
-    "text": "TranscodingStream has two type parameters, C<:Codec and S<:IO, and hence the actual type should be written as TranscodingStream{C<:Codec,S<:IO}. This type wraps an underlying I/O stream S by a codec C. The codec defines transformation (or transcoding) of the stream. For example, when C is a lossless decompression type and S is a file, TranscodingStream{C,S} behaves like a data stream that incrementally decompresses data from the file.Codecs are defined in other packages listed below:CodecZlib.jl\nGizipCompression (GzipCompressionStream)\nGzipDecompression (GzipDecompressionStream)\nZlibCompression (ZlibCompressionStream)\nZlibDecompression (ZlibDecompressionStream)\nDeflateCompression (DeflateCompressionStream)\nDeflateDecompression (DeflateDecompressionStream)\nCodecZngstd.jl\nZstdCompression (ZstdCompressionStream)\nZstdDecompression (ZstdDecompressionStream)\nCodecBzip2.jl\nBzip2Compression (Bzip2CompressionStream)\nBzip2Decompression (Bzip2DecompressionStream)By convention, codec types have a name that matches .*(Co|Deco)mpression and I/O types have a codec name with Stream suffix.  An important thing is these packages depend on TranscodingStreams.jl and not vice versa. This means you can install any codec package you need without installing all codec packages. Also, if you want to define your own codec, it is totally feasible like these packages.  TranscodingStreams.jl requests a codec to implement some interface functions which will be described later."
+    "text": "TranscodingStream has two type parameters, C<:Codec and S<:IO, and hence the actual type should be written as TranscodingStream{C<:Codec,S<:IO}. This type wraps an underlying I/O stream S by a codec C. The codec defines transformation (or transcoding) of the stream. For example, when C is a lossless decompression type and S is a file, TranscodingStream{C,S} behaves like a data stream that incrementally decompresses data from the file.Codecs are defined in other packages listed below:CodecZlib.jl\nGizipCompression (GzipCompressionStream)\nGzipDecompression (GzipDecompressionStream)\nZlibCompression (ZlibCompressionStream)\nZlibDecompression (ZlibDecompressionStream)\nDeflateCompression (DeflateCompressionStream)\nDeflateDecompression (DeflateDecompressionStream)\nCodecZstd.jl\nZstdCompression (ZstdCompressionStream)\nZstdDecompression (ZstdDecompressionStream)\nCodecBzip2.jl\nBzip2Compression (Bzip2CompressionStream)\nBzip2Decompression (Bzip2DecompressionStream)By convention, codec types have a name that matches .*(Co|Deco)mpression and I/O types have a codec name with Stream suffix.  An important thing is these packages depend on TranscodingStreams.jl and not vice versa. This means you can install any codec package you need without installing all codec packages. Also, if you want to define your own codec, it is totally feasible like these packages.  TranscodingStreams.jl requests a codec to implement some interface functions which will be described later."
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "TranscodingStreams.jl",
     "title": "Read lines from a gzip-compressed file",
     "category": "section",
-    "text": "The following snippet is an example of using CodecZlib.jl, which exports GzipDecompressionStream{S} as an alias of TranscodingStream{GzipDecompression,S} where S<:IO:using CodecZlib\nstream = GzipDecompressionStream(open(\"data.gzip\"))\nfor line in eachline(stream)\n    # do something...\nend\nclose(stream)"
+    "text": "The following snippet is an example of using CodecZlib.jl, which exports GzipDecompressionStream{S} as an alias of TranscodingStream{GzipDecompression,S} where S<:IO:using CodecZlib\nstream = GzipDecompressionStream(open(\"data.txt.gz\"))\nfor line in eachline(stream)\n    # do something...\nend\nclose(stream)Note that the last close call will close the file as well.  Alternatively, open(<stream type>, <filepath>) do ... end syntax will close the file at the end:using CodecZlib\nopen(GzipDecompressionStream, \"data.txt.gz\") do stream\n    for line in eachline(stream)\n        # do something...\n    end\nend"
 },
 
 {
@@ -45,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "TranscodingStreams.jl",
     "title": "Save a data matrix with Zstd compression",
     "category": "section",
-    "text": "Writing compressed data is easy. One thing you need to keep in mind is to call close after writing data; otherwise, the output file will be incomplete:using CodecZstd\nmat = randn(100, 100).^2\nfile = open(\"data.mat.zst\", \"w\")\nstream = ZstdCompressionStream(file)\nwritedlm(stream, mat)\nclose(stream)"
+    "text": "Writing compressed data is easy. One thing you need to keep in mind is to call close after writing data; otherwise, the output file will be incomplete:using CodecZstd\nmat = randn(100, 100)\nstream = ZstdCompressionStream(open(\"data.mat.zst\", \"w\"))\nwritedlm(stream, mat)\nclose(stream)Of course, open(<stream type>, ...) do ... end works well:using CodecZstd\nmat = randn(100, 100)\nopen(ZstdCompressionStream, \"data.mat.zst\", \"w\") do stream\n    writedlm(stream, mat)\nend"
 },
 
 {
