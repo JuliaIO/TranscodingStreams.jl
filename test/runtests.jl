4bfab43bca4e98fb86c38c87eeb52acf12186ce4
Kenta Sato <bicycle1885@gmail.com>
2017-09-04 09:26:05 +0900

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -276,7 +276,9 @@ end
     @test eof(stream)  # close
 
     @test_throws ArgumentError NoopStream(IOBuffer(), bufsize=0)
+    @test_throws ArgumentError NoopStream(let s = IOBuffer(); close(s); s; end)
     @test_throws ArgumentError TranscodingStream(Noop(), IOBuffer(), bufsize=0)
+    @test_throws ArgumentError TranscodingStream(Noop(), IOBuffer(), sharedbuf=true)
 end
 
 # This does not implement necessary interface methods.
