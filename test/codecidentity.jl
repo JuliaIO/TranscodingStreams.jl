61868b4f92e5d7cf198dd32085420c919f75fbf6
Kenta Sato <bicycle1885@gmail.com>
2018-03-01 19:49:53 +0900

    remove Identity codec completely

--- test/codecidentity.jl
+++
@@ -1,8 +0,0 @@
-# TODO: Remove this file in the future.
-using TranscodingStreams.CodecIdentity
-@testset "Identity Codec (deprecated)" begin
-    TranscodingStreams.test_roundtrip_transcode(Identity, Identity)
-    TranscodingStreams.test_roundtrip_read(IdentityStream, IdentityStream)
-    TranscodingStreams.test_roundtrip_write(IdentityStream, IdentityStream)
-    TranscodingStreams.test_roundtrip_lines(IdentityStream, IdentityStream)
-end
