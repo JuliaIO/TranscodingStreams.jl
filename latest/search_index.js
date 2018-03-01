c1761740e7ae70cc15ff65e8e45c2409cdf9dc02
autodocs <autodocs>
2018-03-01 11:01:33 +0000

    build based on 049241b

--- latest/search_index.js
+++ latest/search_index.js
@@ -330,31 +330,31 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "devnotes.html#",
-    "page": "Developer's Notes",
-    "title": "Developer's Notes",
+    "page": "Developer\'s Notes",
+    "title": "Developer\'s Notes",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "devnotes.html#Developer's-Notes-1",
-    "page": "Developer's Notes",
-    "title": "Developer's Notes",
+    "location": "devnotes.html#Developer\'s-Notes-1",
+    "page": "Developer\'s Notes",
+    "title": "Developer\'s Notes",
     "category": "section",
     "text": "These notes are not for end users but rather for developers who are interested in the design of the package."
 },
 
 {
     "location": "devnotes.html#TranscodingStream-type-1",
-    "page": "Developer's Notes",
+    "page": "Developer\'s Notes",
     "title": "TranscodingStream type",
     "category": "section",
-    "text": "TranscodingStream{C,S} (defined in src/stream.jl) has three fields:codec: data codec (<:C where C<:Codec)\nstream: data stream (<:S where S<:IO)\nstate: current state (<:State).A codec will be implemented by package developers and only a special codec Noop is defined in this package.  A stream can be any object that implements at least Base.isopen, Base.eof, Base.close, Base.nb_available, Base.unsafe_read, and Base.unsafe_write.  All mutable fields are delegated to state and hence the stream type itself is immutable.A stream has two buffers in the state field. These are used to store pre-transcoded and transcoded data in the stream. The stream passes references of these two buffers to the codec when processing data. The following diagram illustrates the flow of data:When reading data (`state.mode == :read`):\n  user <--- |state.buffer1| <--- <stream.codec> <--- |state.buffer2| <--- stream\n\nWhen writing data (`state.mode == :write`):\n  user ---> |state.buffer1| ---> <stream.codec> ---> |state.buffer2| ---> streamIn the read mode, a user pull out data from state.buffer1 and pre-transcoded data are filled in state.buffer2. In the write mode, a user will push data into state.buffer1 and transcoded data are filled in state.buffer2. The default buffer size is 16KiB for each.State (defined in src/state.jl) has five fields:mode: current stream mode (<:Symbol)\ncode: return code of the last codec's method call (<:Symbol)\nerror: exception returned by the codec (<:Error)\nbuffer1: data buffer that is closer to the user (<:Buffer)\nbuffer2: data buffer that is farther to the user (<:Buffer)The mode field may be one of the following value::idle : initial and intermediate mode, no buffered data\n:read : being ready to read data, data may be buffered\n:write: being ready to write data, data may be buffered\n:stop : transcoding is stopped, data may be buffered\n:close: closed, no buffered data\n:panic: an exception has been thrown in codec, data may be buffered but we           cannot do anythingNote that mode=:stop does not mean there is no data available in the stream. This is because transcoded data may be left in the buffer.The initial mode is :idle and mode transition happens as shown in the following diagram: (Image: Mode transition)Modes surrounded by a bold circle are a state in which the transcoding stream has released resources by calling finalize(codec).  The mode transition should happen in the changemode!(stream, newmode) function in src/stream.jl. Trying an undefined transition will thrown an exception.A transition happens according to internal or external events of the transcoding stream. The status code and the error object returned by codec methods are internal events, and user's method calls are external events.  For example, calling read(stream) will change the mode from :init to :read and then calling close(stream) will change the mode from :read to :close. When data processing fails in the codec, a codec will return :error and the stream will result in :panic."
+    "text": "TranscodingStream{C,S} (defined in src/stream.jl) has three fields:codec: data codec (<:C where C<:Codec)\nstream: data stream (<:S where S<:IO)\nstate: current state (<:State).A codec will be implemented by package developers and only a special codec Noop is defined in this package.  A stream can be any object that implements at least Base.isopen, Base.eof, Base.close, Base.nb_available, Base.unsafe_read, and Base.unsafe_write.  All mutable fields are delegated to state and hence the stream type itself is immutable.A stream has two buffers in the state field. These are used to store pre-transcoded and transcoded data in the stream. The stream passes references of these two buffers to the codec when processing data. The following diagram illustrates the flow of data:When reading data (`state.mode == :read`):\n  user <--- |state.buffer1| <--- <stream.codec> <--- |state.buffer2| <--- stream\n\nWhen writing data (`state.mode == :write`):\n  user ---> |state.buffer1| ---> <stream.codec> ---> |state.buffer2| ---> streamIn the read mode, a user pull out data from state.buffer1 and pre-transcoded data are filled in state.buffer2. In the write mode, a user will push data into state.buffer1 and transcoded data are filled in state.buffer2. The default buffer size is 16KiB for each.State (defined in src/state.jl) has five fields:mode: current stream mode (<:Symbol)\ncode: return code of the last codec\'s method call (<:Symbol)\nerror: exception returned by the codec (<:Error)\nbuffer1: data buffer that is closer to the user (<:Buffer)\nbuffer2: data buffer that is farther to the user (<:Buffer)The mode field may be one of the following value::idle : initial and intermediate mode, no buffered data\n:read : being ready to read data, data may be buffered\n:write: being ready to write data, data may be buffered\n:stop : transcoding is stopped, data may be buffered\n:close: closed, no buffered data\n:panic: an exception has been thrown in codec, data may be buffered but we           cannot do anythingNote that mode=:stop does not mean there is no data available in the stream. This is because transcoded data may be left in the buffer.The initial mode is :idle and mode transition happens as shown in the following diagram: (Image: Mode transition)Modes surrounded by a bold circle are a state in which the transcoding stream has released resources by calling finalize(codec).  The mode transition should happen in the changemode!(stream, newmode) function in src/stream.jl. Trying an undefined transition will thrown an exception.A transition happens according to internal or external events of the transcoding stream. The status code and the error object returned by codec methods are internal events, and user\'s method calls are external events.  For example, calling read(stream) will change the mode from :init to :read and then calling close(stream) will change the mode from :read to :close. When data processing fails in the codec, a codec will return :error and the stream will result in :panic."
 },
 
 {
     "location": "devnotes.html#Shared-buffers-1",
-    "page": "Developer's Notes",
+    "page": "Developer\'s Notes",
     "title": "Shared buffers",
     "category": "section",
     "text": "Adjacent transcoding streams may share their buffers. This will reduce memory allocation and eliminate data copy between buffers.readdata!(input::IO, output::Buffer) and writedata!(output::IO, input::Buffer) do the actual work of read/write data from/to the underlying stream. These methods have a special pass for shared buffers."
@@ -362,7 +362,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "devnotes.html#Noop-codec-1",
-    "page": "Developer's Notes",
+    "page": "Developer\'s Notes",
     "title": "Noop codec",
     "category": "section",
     "text": "Noop (NoopStream) is a codec that does nothing. It works as a buffering layer on top of the underlying stream. Since NoopStream does not need to have two distinct buffers, buffer1 and buffer2 in the State object are shared and some specialized methods are defined for the type. All of these are defined in src/noop.jl."
