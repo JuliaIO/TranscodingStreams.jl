fec368755b53e064d1aa5820deb3109928e13167
Kenta Sato <bicycle1885@gmail.com>
2019-03-22 14:26:35 +0900

    more aggressively inline functions of Memory

--- src/memory.jl
+++ src/memory.jl
@@ -15,26 +15,26 @@ function Memory(data::ByteData)
     return Memory(pointer(data), sizeof(data))
 end
 
-function Base.length(mem::Memory)
+@inline function Base.length(mem::Memory)
     return mem.size
 end
 
-function Base.lastindex(mem::Memory)
+@inline function Base.lastindex(mem::Memory)
     return Int(mem.size)
 end
 
-function Base.checkbounds(mem::Memory, i::Integer)
+@inline function Base.checkbounds(mem::Memory, i::Integer)
     if !(1 ≤ i ≤ lastindex(mem))
         throw(BoundsError(mem, i))
     end
 end
 
-function Base.getindex(mem::Memory, i::Integer)
+@inline function Base.getindex(mem::Memory, i::Integer)
     @boundscheck checkbounds(mem, i)
     return unsafe_load(mem.ptr, i)
 end
 
-function Base.setindex!(mem::Memory, val::UInt8, i::Integer)
+@inline function Base.setindex!(mem::Memory, val::UInt8, i::Integer)
     @boundscheck checkbounds(mem, i)
     return unsafe_store!(mem.ptr, val, i)
 end
