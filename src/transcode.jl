78d2b3bc835422de9689696bbf8208c1d246f6c2
Ben Baumgold <4933671+baumgold@users.noreply.github.com>
2023-04-03 09:42:59 -0400

    Expand ByteData
    Co-authored-by: Mark Kittisopikul <mkitti@users.noreply.github.com>

--- src/transcode.jl
+++ src/transcode.jl
@@ -27,7 +27,7 @@ julia> String(decompressed)
 
 ```
 """
-function Base.transcode(::Type{C}, data::ByteData) where {C<:Codec}
+function Base.transcode(::Type{C}, data::Union{Vector{UInt8},Base.CodeUnits{UInt8}}) where {C<:Codec}
     codec = C()
     initialize(codec)
     try
