9dea17dce81b52eae52f21c67f718b050dc142bf
nhz2 <nhz2@cornell.edu>
2024-03-30 15:57:16 -0400

    use firstindex instead of 1

--- src/stream.jl
+++ src/stream.jl
@@ -412,10 +412,10 @@ function Base.readbytes!(stream::TranscodingStream, b::DenseArray{UInt8}, nb=len
     resized = false
     while filled < nb && !eof(stream)
         if length(b) == filled
-            resize!(b, min(max(length(b) * 2, 8), nb))
+            resize!(b, min(max(length(b) * Int64(2), 8), nb))
             resized = true
         end
-        filled += GC.@preserve b unsafe_read(stream, pointer(b, filled+1), min(length(b), nb)-filled)
+        filled += GC.@preserve b unsafe_read(stream, pointer(b, filled+firstindex(b)), min(length(b), nb)-filled)
     end
     if resized
         resize!(b, filled)
