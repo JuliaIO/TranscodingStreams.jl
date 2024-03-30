fd498814718825c1d536f9e08bb2ac37593cdc96
nhz2 <nhz2@cornell.edu>
2024-03-29 22:34:59 -0400

    only warn if no error

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,8 +1,4 @@
 function Base.seekend(stream::TranscodingStream)
-    @warn """
-    generic `seekend` on a `TranscodingStream` will be removed in a future release.
-    Please use `skip` with a large number instead.
-    """
     mode = stream.state.mode
     if mode == :read
         callstartproc(stream, mode)
@@ -13,5 +9,9 @@ function Base.seekend(stream::TranscodingStream)
         throw_invalid_mode(mode)
     end
     seekend(stream.stream)
+    @warn """
+    generic `seekend` on a `TranscodingStream` will be removed in a future release.
+    Please use `skip` with a large number instead.
+    """
     return stream
 end
