8fdabeda88199cd4298d9cba597e006b1af7655a
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2024-05-04 18:13:50 -0400

    Fix interaction between unread and mark (#208)

--- src/buffer.jl
+++ src/buffer.jl
@@ -14,7 +14,7 @@
 #   position   1   markpos     bufferpos   marginpos  lastindex(data)
 #
 # `markpos` is positive iff there are marked data; otherwise it is set to zero.
-# `markpos` ≤ `bufferpos` ≤ `marginpos` must hold whenever possible.
+# `markpos` ≤ `marginpos` and `bufferpos` ≤ `marginpos` must hold.
 
 mutable struct Buffer
     # data and positions (see above)
@@ -87,6 +87,7 @@ end
 
 function reset!(buf::Buffer)
     @assert buf.markpos > 0
+    @assert buf.markpos ≤ buf.marginpos
     buf.bufferpos = buf.markpos
     buf.markpos = 0
     return buf.bufferpos
@@ -119,7 +120,8 @@ end
 
 # Remove all buffered data.
 function emptybuffer!(buf::Buffer)
-    buf.marginpos = buf.bufferpos
+    buf.markpos = 0
+    buf.bufferpos = buf.marginpos = 1
     return buf
 end
 
@@ -133,16 +135,14 @@ function makemargin!(buf::Buffer, minsize::Integer; eager::Bool = false)
     if marginsize(buf) < minsize || eager
         # datapos refer to the leftmost position of data that must not be
         # discarded. We can left-shift to discard all data before this
-        if buf.markpos == 0
+        datapos = if iszero(buf.markpos)
             # If data is not marked we must not discard buffered (nonconsumed) data
-            datapos = buf.bufferpos
-            datasize = buffersize(buf)
+            buf.bufferpos
         else
-            # Else, we must not consume marked data
-            # (Since markpos ≤ bufferpos, we do not consume buffered data either) 
-            datapos = buf.markpos
-            datasize = buf.marginpos - buf.markpos
+            # Else, we must not consume marked or buffered data
+            min(buf.markpos, buf.bufferpos)
         end
+        datasize = buf.marginpos - datapos
         # Shift data left in buffer to make space for new data
         copyto!(buf.data, 1, buf.data, datapos, datasize)
         shift = datapos - 1
@@ -204,9 +204,21 @@ end
 # Insert data to the current buffer.
 function insertdata!(buf::Buffer, data::Ptr{UInt8}, nbytes::Integer)
     makemargin!(buf, nbytes)
-    copyto!(buf.data, buf.bufferpos + nbytes, buf.data, buf.bufferpos, buffersize(buf))
+    datapos = if iszero(buf.markpos)
+        # If data is not marked we must not discard buffered (nonconsumed) data
+        buf.bufferpos
+    else
+        # Else, we must not consume marked or buffered data
+        min(buf.markpos, buf.bufferpos)
+    end
+    datasize = buf.marginpos - datapos
+    copyto!(buf.data, datapos + nbytes, buf.data, datapos, datasize)
     GC.@preserve buf unsafe_copyto!(bufferptr(buf), data, nbytes)
     supplied!(buf, nbytes)
+    if !iszero(buf.markpos)
+        buf.markpos += nbytes
+    end
+    @assert buf.markpos ≤ buf.marginpos
     return buf
 end
 
