c07f640b6c3e14bb840b56f648c1e0ae9772b1e1
nhz2 <nhz2@cornell.edu>
2025-08-28 09:49:11 -0400

    try resize decode

--- src/chunkcodecs.jl
+++ src/chunkcodecs.jl
@@ -16,11 +16,11 @@ function try_find_decoded_size(::Codec, src::AbstractVector{UInt8})::Nothing
     nothing
 end
 
-function try_decode!(d::Codec, dst::AbstractVector{UInt8}, src::AbstractVector{UInt8}; kwargs...)::MaybeSize
-    try_resize_decode!(d, dst, src, Int64(length(dst)))
+function try_decode!(codec::Codec, dst::AbstractVector{UInt8}, src::AbstractVector{UInt8}; kwargs...)::MaybeSize
+    try_resize_decode!(codec, dst, src, Int64(length(dst)))
 end
 
-function try_resize_decode!(d::Codec, dst::AbstractVector{UInt8}, src::AbstractVector{UInt8}, max_size::Int64; kwargs...)::MaybeSize
+function try_resize_decode!(codec::Codec, dst::AbstractVector{UInt8}, src::AbstractVector{UInt8}, max_size::Int64; kwargs...)::MaybeSize
     dst_size::Int64 = length(dst)
     src_size::Int64 = length(src)
     src_left::Int64 = src_size
@@ -29,40 +29,79 @@ function try_resize_decode!(d::Codec, dst::AbstractVector{UInt8}, src::AbstractV
     check_contiguous(src)
     cconv_src = Base.cconvert(Ptr{UInt8}, src)
     err = Error()
-    # This outer loop is to decode a concatenation of multiple compressed streams.
+    # Outer loop to decode a concatenation of multiple compressed streams.
     while true
-        code = startproc(codec, :write, err)
-        if code === :error
+        if startproc(codec, :write, err) === :error
             @goto handle_error
         end
         if pledgeinsize(codec, src_left, err) === :error
             @goto handle_error
         end
-        # This is the process loop
+        # The process loop
         while true
             GC.@preserve cconv_src begin
-                local src_p = Base.unsafe_convert(Ptr{UInt8}, cconv_src)
+                local src_p = Base.unsafe_convert(Ptr{UInt8}, cconv_src) + (src_size - src_left)
                 local input = Memory(src_p, src_left)
                 # ensure minoutsize is provided
-                local dst_space_needed = minoutsize(d, input)
-                if dst_space_needed > dst_left
+                local dst_space_needed = minoutsize(codec, input)
+                while dst_space_needed > dst_left
                     local next_size = grow_dst!(dst, max_size)
-                    if isnothing(next_size) || 
-                        # Uh oh no more room to grow dst
-                        # but we don't know if the processing is done.
-                        # Allocate some extra output to check.
-                        local scratch_dst = zeros(UInt8, dst_space_needed)
+                    if isnothing(next_size)
+                        break
                     end
                     dst_left += next_size - dst_size
                     dst_size = next_size
                     @assert dst_left > 0
-
                 end
-                # dst may get resized, so cconvert needs to be redone on each iteration.
                 cconv_dst = Base.cconvert(Ptr{UInt8}, dst)
                 GC.@preserve cconv_dst begin
-                    local dst_p = Base.unsafe_convert(Ptr{UInt8}, cconv_dst)
-                    local output = Memory(dst_p, dst_left)
+                    local dst_p = Base.unsafe_convert(Ptr{UInt8}, cconv_dst) + (dst_size - dst_left)
+                    if dst_space_needed > dst_left
+                        # Try to do the decoding into a scratch buffer
+                        # The scratch buffer should typically be just a few bytes.
+                        local scratch_dst = zeros(UInt8, dst_space_needed)
+                        local cconv_scratch_dst = Base.cconvert(Ptr{UInt8}, scratch_dst)
+                        GC.@preserve cconv_scratch_dst let
+                            local scratch_p = Base.unsafe_convert(Ptr{UInt8}, cconv_scratch_dst)
+                            local output = Memory(scratch_p, dst_space_needed)
+                            local Δin, Δout, code = process(codec, input, output, err)
+                            if code === :error
+                                @goto handle_error
+                            end
+                            local valid_Δout = min(Δout, dst_left)
+                            src_left -= Δin
+                            unsafe_copyto!(dst_p, scratch_p, valid_Δout)
+                            if Δout > dst_left
+                                # Ran out of dst space
+                                return NOT_SIZE
+                            end
+                            dst_left -= Δout
+                            if code === :end
+                                if src_left > 0
+                                    break # out of the process loop to decode next stream
+                                else
+                                    # Done
+                                    return dst_size - dst_left
+                                end
+                            end
+                        end
+                    else
+                        local output = Memory(dst_p, dst_left)
+                        local Δin, Δout, code = process(codec, input, output, err)
+                        if code === :error
+                            @goto handle_error
+                        end
+                        src_left -= Δin
+                        dst_left -= Δout
+                        if code === :end
+                            if src_left > 0
+                                break # out of the process loop to decode next stream
+                            else
+                                # Done
+                                return dst_size - dst_left
+                            end
+                        end
+                    end
                 end
             end
         end
